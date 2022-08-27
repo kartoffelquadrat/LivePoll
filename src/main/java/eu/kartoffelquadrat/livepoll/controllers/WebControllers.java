e5905f9f48a15fa6a3c62511246bec0319ed0830
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-08-27 13:01:34 -0400

    implemented generi qr file tunnel endpoint

--- src/main/java/eu/kartoffelquadrat/livepoll/controllers/WebControllers.java
+++ src/main/java/eu/kartoffelquadrat/livepoll/controllers/WebControllers.java
@@ -5,12 +5,19 @@ import eu.kartoffelquadrat.livepoll.PollLauncher;
 import eu.kartoffelquadrat.livepoll.PollManager;
 import eu.kartoffelquadrat.livepoll.pollutils.AlphabetSanitizer;
 import eu.kartoffelquadrat.livepoll.pollutils.Hyphenizer;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebControllers {
@@ -76,13 +83,46 @@ public class WebControllers {
    * violation to the REST style, but since we want to support vote by QR scanning it has to be GET
    * (default HTTP method for browser resource access).
    */
-  @RequestMapping("/polls/{pollid}/{option}")
+  @GetMapping("/polls/{pollid}/{option}")
   public String registerVote(@PathVariable("pollId") String pollId,
                              @PathVariable("option") String option) {
 
     pollManager.getPollByIdentifier(pollId).voteForOption(option);
     return "I registered your vote for \"" + option +
         "\". Thank you for your participation. You can leave this page now.";
+  }
+
+
+  /**
+   * Helper method to access QR code png files stored on disk. Local references are blockedc by
+   * browsers security policy so we need an HTTP tunnel.
+   *
+   * @param pollid  as the id of the qr code option to look up
+   * @param option  as the selected vote option
+   * @param request as the connection http opeject, required to identify connects form other
+   *                machines and reject them.
+   * @return ByteStream of QR png with required Meta media type.
+   * @throws IOException in case the file is not found / cannot be read.
+   */
+  @GetMapping(
+      value = "/polls/{pollid}/qr/{option}",
+      produces = MediaType.IMAGE_PNG_VALUE
+  )
+  public @ResponseBody
+  byte[] getImageWithMediaType(@PathVariable("pollid") String pollid,
+                               @PathVariable("option") String option, HttpServletRequest request)
+      throws IOException {
+
+    // dont accept calls from elsewhere
+    if (!isCallFromLocalhost(request)) {
+      return null;
+    }
+
+    // Return the referenced QR code
+    FileInputStream in = new FileInputStream(
+        PollLauncher.pollTmpDir + pollid + "-" + option +
+            ".png");
+    return IOUtils.toByteArray(in);
   }
 
   /**
