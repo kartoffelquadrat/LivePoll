dbc8d6bd36e20d68d5e3b08ab0c32fc5d845b94a
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-08-27 11:27:55 -0400

    advanced layout qr page

--- src/main/resources/static/fillpollpage.js
+++ src/main/resources/static/fillpollpage.js
@@ -4,9 +4,20 @@ function fillWithPollDetails() {
 
 
     // use poll details to set: title, QRs, counters
-    setTitle(pollDetails);
+    // setTitle(pollDetails);
+    reduceToTwoColumns();
+}
+
+function reduceToTwoColumns() {
+
+    // make middle column invisible
+    document.getElementById("middle").style.display = "none";
+
+    // boost width of remaining columns to 50%
+    document.getElementById("left").style.width = "50%";
+    document.getElementById("right").style.width = "50%";
 }
 
 function setTitle() {
-    alert("TITLE");
+    // alert("TITLE");
 }
