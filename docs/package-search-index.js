c18b5def90960c30c33604e07bd3102f577101bf
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-08 16:20:53 -0400

    Configured javadoc plugin to generate javadoc into docs folder, default for github pages

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"eu.kartoffelquadrat.livepoll"},{"l":"eu.kartoffelquadrat.livepoll.controllers"},{"l":"eu.kartoffelquadrat.livepoll.pollutils"},{"l":"eu.kartoffelquadrat.livepoll.qrgenerator"}]
